547b04c2a98672c229226a18c837c5761140ba40
catarium <tartarxan@gmail.com>
2022-01-11 23:19:16 +0300

    secured from github api limit

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ async function loadStars() {
     els = [].slice.call(document.getElementsByClassName('project-stars-count'))
     console.log(els)
     for (var el in els) {
-        res = await fetch(`https://api.github.com/repos/catarium/${els[el].dataset.repo}`, {'method': "GET"});
-        els[el].innerHTML =  '✩ ' + (await res.json()).stargazers_count;
+        res = await fetch(`https://api.gidthub.com/repos/catarium/${els[el].dataset.repo}`, {'method': "GET"});
+        res = await res.json()
+        if (res.stargazers_count != undefined) {
+            els[el].innerHTML =  '✩ ' + res.stargazers_count;
+        }
+
     };
 }
