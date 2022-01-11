ce562d962aad62e1ec91540c8e68a5303312a09b
catarium <tartarxan@gmail.com>
2022-01-12 00:26:47 +0300

    bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function loadStars() {
     els = [].slice.call(document.getElementsByClassName('project-stars-count'))
     console.log(els)
     for (var el in els) {
-        res = await fetch(`https://api.gidthub.com/repos/catarium/${els[el].dataset.repo}`, {'method': "GET"});
+        res = await fetch(`https://api.github.com/repos/catarium/${els[el].dataset.repo}`, {'method': "GET"});
         res = await res.json()
         if (res.stargazers_count != undefined) {
             els[el].innerHTML =  '✩ ' + res.stargazers_count;
